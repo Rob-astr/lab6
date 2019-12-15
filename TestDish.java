5a889b189d37476ec87e5e306f4c8ac76cedbfc0
Rob-astr <58914867+Rob-astr@users.noreply.github.com>
2019-12-15 21:02:53 +0300

    Add files via upload

---
+++ TestDish.java
@@ -0,0 +1,8 @@
+package lab5;
+
+public class TestDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Жело", "красный", "маленькая");
+        System.out.println(t);
+    }
+}
